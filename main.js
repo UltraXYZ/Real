377e16a16c30999c820932eb48e56b4f9f4a799e
UltraXYZ <79716752+UltraXYZ@users.noreply.github.com>
2021-11-26 19:53:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Loaded!');
+}
+
+function draw() {
+    background('#ff7b00');
+
+    document.getElementById("square_sides").innerHTML = "Width and Height of the = " + difference + "px";
+    fill('#ff00e6');
+    stroke('#ff00e6');
+    square(noseX, noseY, difference);
+    textSize("Book Club is Open", noseX, noseY, leftWristX, rightWristX)
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
